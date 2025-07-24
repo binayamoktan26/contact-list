dfeebfad6a66cceb63f89dfbf15a11b389c37723
nawang tashi lama <binayamoktan26@gmail.com>
2025-07-24 23:29:27 +0545

    displaying homeScreen to appScreen

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+// slide to next page
+const sliderEle = document.getElementById("mySlider");
+
+sliderEle.addEventListener("change", (e) => {
+  const { value } = e.target;
+  const label = document.getElementById("label");
+
+  if (value > 80) {
+    label.textContent = "";
+    displayAppScreen();
+  } else {
+    label.textContent = "Slide To Unlock";
+  }
+});
+
+const displayAppScreen = () => {
+  //hide home screen
+  document.querySelector(".homeScreen").remove();
+  // display app screen
+  document.querySelector(".appScreen").style.display = "block";
+};
