38c9d7d3020299ff3b0df73e2ca4318890a00224
nawang tashi lama <binayamoktan26@gmail.com>
2025-08-04 23:54:50 +0545

    ending api

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const apiEp = "https://randomuser.me/api?results=3";
+let userlist = [];
 // slide to next page
 const sliderEle = document.getElementById("mySlider");
 
@@ -19,3 +21,25 @@ const displayAppScreen = () => {
   // display app screen
   document.querySelector(".appScreen").style.display = "block";
 };
+
+const fetchUser = async (url) => {
+  //appling fetch API to get data
+  // promise method
+  // fetch(url)
+  //   .then((response) => {
+  //     console.log(response);
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     console.log(data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+  //async / await methd
+  const response = await fetch(url);
+  const data = await response.json();
+  userList = data.results;
+  console.log(userList);
+};
+fetchUser(apiEp);
